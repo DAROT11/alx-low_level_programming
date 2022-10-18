05e4b2521fed71fa724041c72060013dcd3dfb1e
DAROT11 <ojojamesayanfe@gmail.com>
2022-10-18 13:05:32 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *main - Prints "Holberton" followed by a new line.
- * Return: Always 0.
+ * main - Prints Holberton as a message.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+
+	{
+		_putchar(str[count]);
+	}
 	_putchar('\n');
 	return (0);
 }
