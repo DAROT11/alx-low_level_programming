13f2488ac2127e440d11a6c3684ade51b37ac99a
DAROT11 <ojojamesayanfe@gmail.com>
2022-10-20 14:28:56 -0700

    task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - cause an infinite loop
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -21,3 +21,4 @@ int main(void)
 	printf("infinite loop avoided! \\o/\n");
 
 	return (0);	
+}
