bab2b71238d564ab3c8c14b5224cfd3021ad339b
DAROT11 <ojojamesayanfe@gmail.com>
2022-11-01 18:05:28 -0700

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,19 +9,12 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int index;
+
+	for( index = 0; s[index] >= '\0'; index++;)
 	{
-		if (*s == c)
-			return (s);
-		s++;
+		if (s[index] == c)
+			return (s + index);
 	}
-	/**
-	 * if c is '\0', you should return
-	 * the pointer to the '\0' of the
-	 * string s
-	*/
-	if (*s == c)
-		return (s);
-	/ return null if not found /
 	return ('\0');
 }
